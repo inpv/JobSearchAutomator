e69274a2345d2128f1cd8ad2798c90cdd8767b91
inpv <inpv@protonmail.ch>
2022-11-10 01:44:48 +0300

    - Add allure decorators - Update selenium requirements

--- tests/test_cv_autoupdate.py
+++ tests/test_cv_autoupdate.py
@@ -1,26 +1,37 @@
+import allure
 from utils.crypt import Crypt
 from datas.login_datas import LoginDatas
 from datas.webpage_datas import HeadHunterLocators
 from datas.connection_datas import ConnectionDatas
 
 
+@allure.suite('Test CV update')
 class TestCvAutoupdate:
 
     @staticmethod
+    @allure.title('Test CV update')
     def test_cv_autoupdate(browser, hh_page):
         for page in ConnectionDatas.PAGE_ADDRESSES:
-            hh_page.go_to_page(page)
+            with allure.step('Open hh page'):
+                hh_page.go_to_page(page)
 
-            hh_page.click_on_button(HeadHunterLocators.LOCATOR_HH_ALLCORRECT_BUTTON)
-            hh_page.click_on_button(HeadHunterLocators.LOCATOR_HH_LOGIN_LINK)
+            with allure.step('Confirm your city'):
+                hh_page.click_on_button(HeadHunterLocators.LOCATOR_HH_ALLCORRECT_BUTTON)
 
-            hh_page.click_on_button(HeadHunterLocators.LOCATOR_HH_BYPASSWORD_BUTTON)
+            with allure.step('Click login button'):
+                hh_page.click_on_button(HeadHunterLocators.LOCATOR_HH_LOGIN_LINK)
 
-            hh_page.enter_word_into_input(HeadHunterLocators.LOCATOR_HH_LOGININPUT_FIELD,
-                                          LoginDatas.LOGIN_ENCODED_STR)
-            hh_page.enter_word_into_input(HeadHunterLocators.LOCATOR_HH_PASSWORDINPUT_FIELD,
-                                          str(Crypt.get_random_decrypted_pass()))
+            with allure.step('Choose registration by login instead of phone number'):
+                hh_page.click_on_button(HeadHunterLocators.LOCATOR_HH_BYPASSWORD_BUTTON)
 
-            hh_page.click_on_button(HeadHunterLocators.LOCATOR_HH_LOGINSUBMIT_BUTTON)
+            with allure.step('Enter login data into login field'):
+                hh_page.enter_word_into_input(HeadHunterLocators.LOCATOR_HH_LOGININPUT_FIELD,
+                                              LoginDatas.LOGIN_ENCODED_STR)
+            with allure.step('Enter passwd data into passwd field'):
+                hh_page.enter_word_into_input(HeadHunterLocators.LOCATOR_HH_PASSWORDINPUT_FIELD,
+                                              str(Crypt.get_random_decrypted_pass()))
+            with allure.step('Submit login and password data'):
+                hh_page.click_on_button(HeadHunterLocators.LOCATOR_HH_LOGINSUBMIT_BUTTON)
 
-            hh_page.click_on_buttons(HeadHunterLocators.LOCATOR_HH_RESUMEUPDATE_BUTTON)
+            with allure.step('Click all update buttons'):
+                hh_page.click_on_buttons(HeadHunterLocators.LOCATOR_HH_RESUMEUPDATE_BUTTON)
