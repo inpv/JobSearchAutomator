ad27a835c741c9b60d8dbfcc02a00ad4285a8b3e
inpv <inpv@protonmail.ch>
2026-03-02 22:40:21 +0500

    - Change update button's locator selector for the new layout

--- datas/webpage_datas.py
+++ datas/webpage_datas.py
@@ -13,5 +13,5 @@ class HeadHunterLocators:
     LOCATOR_HH_CAPTCHA_FIELD = (By.CSS_SELECTOR, "input[data-qa='account-captcha-input']")
     LOCATOR_HH_EMPLOYER_REVIEW_CLOSE_BUTTON = (By.CSS_SELECTOR, "div[data-qa='notification-close-button']")
     LOCATOR_HH_SERVICE_REVIEW_CLOSE_BUTTON = (By.CLASS_NAME, "uxs-1h3RIxayGx")
-    LOCATOR_HH_RESUMEUPDATE_BUTTON = (By.CSS_SELECTOR, "button[data-qa='resume-update-button_actions']")
+    LOCATOR_HH_RESUMEUPDATE_BUTTON = (By.CSS_SELECTOR, "span[data-qa='resume-update-button-text']")
     LOCATOR_HH_RESUMEUPDATE_BUTTONTEXT = "Поднять в поиске"
