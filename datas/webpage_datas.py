206bf0a9764c66b6c508dbaf5d2fee9847eaa259
inpv <inpv@protonmail.ch>
2026-06-16 12:00:00 +0500

    - Change submit button locator to a correct one

--- datas/webpage_datas.py
+++ datas/webpage_datas.py
@@ -10,7 +10,7 @@ class HeadHunterLocators:
     LOCATOR_HH_BYPASSWORD_BUTTON = (By.CSS_SELECTOR, "button[data-qa='expand-login-by-password']")
     LOCATOR_HH_LOGININPUT_FIELD = (By.CSS_SELECTOR, "input[data-qa='applicant-login-input-email']")
     LOCATOR_HH_PASSWORDINPUT_FIELD = (By.CSS_SELECTOR, "input[data-qa='applicant-login-input-password']")
-    LOCATOR_HH_LOGINSUBMIT_BUTTON = (By.CSS_SELECTOR, "button[data-qa='account-login-submit']")
+    LOCATOR_HH_LOGINSUBMIT_BUTTON = (By.CSS_SELECTOR, "button[data-qa='submit-button']")
     LOCATOR_HH_CAPTCHA_FIELD = (By.CSS_SELECTOR, "input[data-qa='account-captcha-input']")
     LOCATOR_HH_EMPLOYER_REVIEW_CLOSE_BUTTON = (By.CSS_SELECTOR, "div[data-qa='notification-close-button']")
     LOCATOR_HH_SERVICE_REVIEW_CLOSE_BUTTON = (By.CLASS_NAME, "uxs-1h3RIxayGx")
